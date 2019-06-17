8e62b3b0e11b98da4c8010b55aede7bfc95af3bc
qrtjslucas <qrtjslucas@DESKTOP-475RLVD>
2019-06-17 14:01:54 -0300

    commit inicial

---
+++ biblioteca/FuncoesBasicas/InicializadorKinect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+
+namespace biblioteca.FuncoesBasicas
+{
+    class InicializadorKinect
+    {
+        public static KinectSensor InicializarPrimeiroSensor(int anguloElevacaoInicial)
+        {
+            KinectSensor kinect = KinectSensor.KinectSensors.First(sensor => sensor.Status == KinectStatus.Connected); kinect.Start(); kinect.ElevationAngle = anguloElevacaoInicial;
+            return kinect;
+
+
+        }   
+
+
+
+
+    }
+}
+
+
